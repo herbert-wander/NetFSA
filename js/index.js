7636b22771253c4d07641e5f332bfc1521a72876
Herbert Wander "Dragon <herbert.starcraft2@gmail.com>
2022-05-02 02:55:41 -0300

    New Imgs, Initial FAQ Implementation and initial JS setup

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-function mapOverlayClear()
-{
+window.onload = setupFunctions;
+
+function mapOverlayClear() {
     document.getElementById("overlayMap").style.pointerEvents = "none";
     document.getElementById("overlayMap").style.display = "none";
+}
+
+function setupFunctions() {
+    var arrowClickElements = Array.from(document.getElementsByClassName("arrowClick"));
+    arrowClickElements.forEach(showAnswerAddListener);
+}
+
+function showAnswerAddListener(item) {
+    item.addEventListener("click", showAnswer);
+}
+
+function showAnswer(e) {
+    if (e.target.classList.contains("arrowClick")) {
+        if (window.getComputedStyle((e.target.parentElement.nextElementSibling)).display == "none") {
+            e.target.parentElement.nextElementSibling.style.display = "block";
+        } else {
+            e.target.parentElement.nextElementSibling.style.display = "none";
+        }
+    }
+
 }
