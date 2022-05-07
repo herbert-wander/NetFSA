418711f4530a8906688a5392143cb6dcf11585a4
Herbert Wander "Dragon <herbert.starcraft2@gmail.com>
2022-05-07 19:45:22 -0300

    SlideShow parcial Implementation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.onload = setupFunctions;
+$(document).ready(clientesSlide);
 
 function mapOverlayClear() {
     document.getElementById("overlayMap").style.pointerEvents = "none";
@@ -12,20 +13,15 @@ function setupFunctions() {
 }
 
 function showAllDeals(e) {
-    //document.getElementsByTagName("body")[0].style.height = "100%";
     document.getElementById("welcome").style.display = "none";
     document.getElementById("mainGradient").style.display = "none";
-    //document.getElementsByTagName("body")[0].style.overflowY = "hidden";
-    document.getElementById("headerNav").style.width = "100%";
-    document.getElementById("headerNav").style.position = "fixed";
-    document.getElementById("headerNav").style.top = 0;
+    window.scrollTo(0, 0);
+    document.getElementById("dealsFrame").style.display = "flex";
     document.getElementById("dealsFrame").style.visibility = "visible";
     var navHeight = window.getComputedStyle(document.getElementById("headerNav")).height.replace("px", "");
     //var moreOffersHeight = (100 - (navHeight / (window.innerHeight / 100))) + "vh"
     //var rounded = Math.round(number * 10) / 10
     document.getElementById("dealsFrame").style.top = (navHeight + 1) + "px";
-    //document.getElementById("dealsFrame").style.height = "100%";
-
 }
 
 function showAnswerAddListener(item) {
@@ -41,4 +37,38 @@ function showAnswer(e) {
         }
     }
 
+}
+
+function clientesSlide() {
+    setInterval(animationOp, 13000);
+    var words = $("[data-words]").attr("data-words").split("|");
+    var wordsSize = words.length;
+
+    var self = $("[data-words]");
+    var selfRet = $("[data-retratos]");
+
+    self.append($('<span id="depoTextSpan0">' + words[0] + '</span>'));
+    selfRet.append($('<img id="retCli0" alt="" src="imgs/retratos' + 0 + '.png"></img>'));
+
+    for (var i = 1; i < wordsSize; i++) {
+        self.append(($('<span id="depoTextSpan' + i + '">' + words[i] + '</span>')).hide());
+        selfRet.append(($('<img id="retCli' + i + '" alt="" src="imgs/retratos' + i + '.png"></img>')).hide());
+    }
+}
+
+function animationOp() {
+    var loopNumber = 3;
+    var idElementoText = "depoTextSpan" + countID;
+    var idElementoRet = "retCli" + countID;
+    $(document.getElementById(idElementoRet)).fadeOut(500);
+    $(document.getElementById(idElementoText)).fadeOut(500, function () {
+        countID++;
+        if (countID == loopNumber) {
+            countID = 0;
+        }
+        idElementoText = "depoTextSpan" + countID;
+        $(document.getElementById(idElementoText)).fadeIn(1000);
+        idElementoRet = "retCli" + countID;
+        $(document.getElementById(idElementoRet)).fadeIn(1000);
+    });
 }
