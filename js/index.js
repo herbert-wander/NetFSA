b41bf86badba741f7f2d24abb8c2b28a2551423d
Herbert Wander "Dragon <herbert.starcraft2@gmail.com>
2022-05-10 13:54:56 -0300

    Deals Frame final Implementation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 window.onload = setupFunctions;
-$(document).ready(clientesSlide);
 
 function mapOverlayClear() {
     document.getElementById("overlayMap").style.pointerEvents = "none";
@@ -9,19 +8,35 @@ function mapOverlayClear() {
 function setupFunctions() {
     var arrowClickElements = Array.from(document.getElementsByClassName("arrowClick"));
     document.getElementById("moreOffers").addEventListener("click", showAllDeals);
+    document.getElementById("closeDealsFrame").addEventListener("click", closeAllDeals);
     arrowClickElements.forEach(showAnswerAddListener);
 }
 
 function showAllDeals(e) {
-    document.getElementById("welcome").style.display = "none";
-    document.getElementById("mainGradient").style.display = "none";
-    window.scrollTo(0, 0);
+    //document.getElementById("welcome").style.display = "none";
+    //document.getElementById("mainGradient").style.display = "none";
+    //window.scrollTo(0, 0);
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
     document.getElementById("dealsFrame").style.display = "flex";
     document.getElementById("dealsFrame").style.visibility = "visible";
+    document.getElementById("dealsFrame").style.overflowY = "scroll";
+    document.getElementById("dealsFrame").classList.remove("closeDealSlideFX");
+    document.getElementById("closeDealsFrame").classList.remove("closeDealSlideButtonFX");
+    document.getElementById("dealsFrame").classList.add("dealSlideFX");
+    document.getElementById("closeDealsFrame").classList.add("dealSlideButtonFX");
     var navHeight = window.getComputedStyle(document.getElementById("headerNav")).height.replace("px", "");
     //var moreOffersHeight = (100 - (navHeight / (window.innerHeight / 100))) + "vh"
     //var rounded = Math.round(number * 10) / 10
-    document.getElementById("dealsFrame").style.top = (navHeight + 1) + "px";
+    //document.getElementById("dealsFrame").style.top = (navHeight + 1) + "px";
+}
+
+function closeAllDeals(e) {
+    document.getElementById("dealsFrame").classList.add("closeDealSlideFX");
+    document.getElementById("closeDealsFrame").classList.add("closeDealSlideButtonFX");
+    document.getElementById("dealsFrame").classList.remove("dealSlideFX");
+    document.getElementById("closeDealsFrame").classList.remove("dealSlideButtonFX");
+    document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+    document.getElementById("dealsFrame").style.overflowY = "hidden";
 }
 
 function showAnswerAddListener(item) {
@@ -37,38 +52,4 @@ function showAnswer(e) {
         }
     }
 
-}
-
-function clientesSlide() {
-    setInterval(animationOp, 13000);
-    var words = $("[data-words]").attr("data-words").split("|");
-    var wordsSize = words.length;
-
-    var self = $("[data-words]");
-    var selfRet = $("[data-retratos]");
-
-    self.append($('<span id="depoTextSpan0">' + words[0] + '</span>'));
-    selfRet.append($('<img id="retCli0" alt="" src="imgs/retratos' + 0 + '.png"></img>'));
-
-    for (var i = 1; i < wordsSize; i++) {
-        self.append(($('<span id="depoTextSpan' + i + '">' + words[i] + '</span>')).hide());
-        selfRet.append(($('<img id="retCli' + i + '" alt="" src="imgs/retratos' + i + '.png"></img>')).hide());
-    }
-}
-
-function animationOp() {
-    var loopNumber = 3;
-    var idElementoText = "depoTextSpan" + countID;
-    var idElementoRet = "retCli" + countID;
-    $(document.getElementById(idElementoRet)).fadeOut(500);
-    $(document.getElementById(idElementoText)).fadeOut(500, function () {
-        countID++;
-        if (countID == loopNumber) {
-            countID = 0;
-        }
-        idElementoText = "depoTextSpan" + countID;
-        $(document.getElementById(idElementoText)).fadeIn(1000);
-        idElementoRet = "retCli" + countID;
-        $(document.getElementById(idElementoRet)).fadeIn(1000);
-    });
 }
