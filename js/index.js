971de8e37d75fb10dabd4b4f3037d9d1c76e53d2
Herbert Wander "Dragon <herbert.starcraft2@gmail.com>
2022-05-10 21:08:54 -0300

    Slideshow Responsive implemented, Footer implemented, Links Fixed, Favicon Done

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
 window.onload = setupFunctions;
+window.onresize = resizeSlideshow;
 
 function mapOverlayClear() {
     document.getElementById("overlayMap").style.pointerEvents = "none";
     document.getElementById("overlayMap").style.display = "none";
 }
 
+function resizeSlideshow() {
+    document.getElementById("welcome").style.maxHeight = document.body.clientWidth / 2.66666;
+}
+
 function setupFunctions() {
+    document.getElementById("welcome").style.maxHeight = document.body.clientWidth / 2.66666;
     var arrowClickElements = Array.from(document.getElementsByClassName("arrowClick"));
+    arrowClickElements.forEach(showAnswerAddListener);
+    var navFixElements = Array.from(document.getElementsByClassName("navScrollFix"));
+    navFixElements.forEach(navFixAddListener);
     document.getElementById("moreOffers").addEventListener("click", showAllDeals);
     document.getElementById("closeDealsFrame").addEventListener("click", closeAllDeals);
-    arrowClickElements.forEach(showAnswerAddListener);
+
 }
 
 function showAllDeals(e) {
-    //document.getElementById("welcome").style.display = "none";
-    //document.getElementById("mainGradient").style.display = "none";
-    //window.scrollTo(0, 0);
     document.getElementsByTagName("body")[0].style.overflow = "hidden";
     document.getElementById("dealsFrame").style.display = "flex";
     document.getElementById("dealsFrame").style.visibility = "visible";
@@ -24,7 +30,7 @@ function showAllDeals(e) {
     document.getElementById("closeDealsFrame").classList.remove("closeDealSlideButtonFX");
     document.getElementById("dealsFrame").classList.add("dealSlideFX");
     document.getElementById("closeDealsFrame").classList.add("dealSlideButtonFX");
-    var navHeight = window.getComputedStyle(document.getElementById("headerNav")).height.replace("px", "");
+    //var navHeight = window.getComputedStyle(document.getElementById("headerNav")).height.replace("px", "");
     //var moreOffersHeight = (100 - (navHeight / (window.innerHeight / 100))) + "vh"
     //var rounded = Math.round(number * 10) / 10
     //document.getElementById("dealsFrame").style.top = (navHeight + 1) + "px";
@@ -43,13 +49,23 @@ function showAnswerAddListener(item) {
     item.addEventListener("click", showAnswer);
 }
 
-function showAnswer(e) {
-    if (e.target.classList.contains("arrowClick")) {
-        if (window.getComputedStyle((e.target.parentElement.nextElementSibling)).display == "none") {
-            e.target.parentElement.nextElementSibling.style.display = "block";
-        } else {
-            e.target.parentElement.nextElementSibling.style.display = "none";
-        }
-    }
+function navFixAddListener(item) {
+    item.addEventListener("click", navFixScroll);
+}
 
+function showAnswer(e) {
+    if (window.getComputedStyle((e.target.parentElement.nextElementSibling)).display == "none") {
+        e.target.parentElement.nextElementSibling.style.display = "block";
+    } else {
+        e.target.parentElement.nextElementSibling.style.display = "none";
+    }
+}
+
+function navFixScroll(e) {
+    e.preventDefault();
+    var navHeight = window.getComputedStyle(document.getElementById("headerNav")).height.replace("px", "");
+    var scrollPos = document.getElementById(e.target.getAttribute("href").replace("#", "")).offsetTop - navHeight;
+    console.log(scrollPos);
+    console.log(navHeight);
+    window.scroll(0, scrollPos);
 }
