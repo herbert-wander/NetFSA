8f0f9e24a33d169d1e3a4a919eb316951f125f41
Herbert Wander "Dragon <herbert.starcraft2@gmail.com>
2023-01-17 11:35:49 -0300

    Dynamic Pricing Plans Feature

--- js/index.js
+++ js/index.js
@@ -16,8 +16,8 @@ function resizeSlideshow() {
     document.getElementById("welcome").style.maxHeight = Math.floor(document.body.clientWidth / 2.66);
 }
 
-function setupFunctions() {
-
+async function setupFunctions() {
+    await getRemotePlansData();
     let observer = new IntersectionObserver(aboutUsAnimation, options);
     observer.observe(document.getElementById("aboutUs"));
 
@@ -49,6 +49,123 @@ function setupFunctions() {
     document.getElementById("pageLoader").onanimationend = () => {
         document.getElementById("pageLoader").style.display = "none";
     };
+}
+
+async function getRemotePlansData() {
+    await fetch("https://raw.githubusercontent.com/herbert-wander/NetFSAData/main/priceplansdata.json")
+        .then(data => data.json())
+        .then(data => {
+            setMainCards(data);
+            setPlansCardList(data);
+        })
+        .catch(error => console.log("Error = " + error));
+}
+
+function setMainCards(data) {
+    let plansData = [];
+    let scanPos = 0;
+    while (plansData.length < 3 && scanPos < data.planos.length) {
+        if (data.planos[scanPos].nivelDeDestaque == 1 || data.planos[scanPos].nivelDeDestaque == 2) {
+            plansData.push(data.planos[scanPos]);
+        }
+        scanPos++;
+    }
+    if (plansData[0].nivelDeDestaque == 2) {
+        let replaceHtml = document.getElementById("offerCard02").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("450 megas", plansData[0].nome)
+        document.getElementById("offerCard02").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[0].preco;
+
+        replaceHtml = document.getElementById("offerCard01").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("50 megas", plansData[1].nome)
+        document.getElementById("offerCard01").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[1].preco;
+
+        replaceHtml = document.getElementById("offerCard03").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("90 megas", plansData[2].nome)
+        document.getElementById("offerCard03").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[2].preco;
+    } else if (plansData[1].nivelDeDestaque == 2) {
+        let replaceHtml = document.getElementById("offerCard02").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("450 megas", plansData[1].nome)
+        document.getElementById("offerCard02").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[1].preco;
+
+        replaceHtml = document.getElementById("offerCard01").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("50 megas", plansData[2].nome)
+        document.getElementById("offerCard01").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[2].preco;
+
+        replaceHtml = document.getElementById("offerCard03").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("90 megas", plansData[0].nome)
+        document.getElementById("offerCard03").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[0].preco;
+    } else {
+        let replaceHtml = document.getElementById("offerCard02").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("450 megas", plansData[2].nome)
+        document.getElementById("offerCard02").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[2].preco;
+
+        replaceHtml = document.getElementById("offerCard01").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("50 megas", plansData[1].nome)
+        document.getElementById("offerCard01").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[1].preco;
+
+        replaceHtml = document.getElementById("offerCard03").getElementsByTagName("h2")[0];
+        replaceHtml.innerHTML = replaceHtml.innerHTML.replace("90 megas", plansData[0].nome)
+        document.getElementById("offerCard03").getElementsByClassName("priceCard")[0].innerHTML = "R$ " + plansData[0].preco;
+    }
+}
+
+function setPlansCardList(data) {
+    replaceHtml = document.getElementById("cardsDealContainer");
+    let htmlPlansData = "";
+    data.planos.forEach(element => {
+        if (element.temTvGratis) {
+            htmlPlansData += `
+                <div class="cardDealOffer flex">
+                    <div class="titleOfferContainer flex">
+                        <span class = "dealTitleOffer"> ` + element.nome + ` <br> fibra óptica </span>
+                        <img src="imgs/MoreOffers_Arrow.svg" alt="">
+                    </div>
+                    <div class="dealsFeaturesIconsBox flex">
+                        <img src="imgs/OfferCardIcon_SmartPhone.svg" alt="">
+                        <img src="imgs/OfferCardIcon_Computer.svg" alt="">
+                        <img src="imgs/OfferCardIcon_GameConsole.svg" alt="">
+                        <img src="imgs/OfferCardIcon_HDVideo.svg" alt="">
+                        <img src="imgs/MoreOffers_Wifi.svg" alt="">
+                        <span class="dealPlusSign">+</span>
+                        <img src="imgs/MoreOffers_Tv.svg" alt="">
+                        <span>Gratis</span>
+                    </div>
+                    <div class="dealPriceContract flex">
+                        <span>por apenas</span>
+                        <span class = "dealPriceExta"> R$ ` + element.preco + ` </span>
+                        <a class="dealButton"
+                            href="https://api.whatsapp.com/send?phone=557530222334&text=Olá! Gostaria de contratar um produto com vocês, como podemos prosseguir?"
+                            target="_blank">Contratar</a>
+                    </div>
+                </div>
+            `;
+        } else {
+            htmlPlansData += `
+                <div class = "cardDealOffer flex  fix35Card">
+                    <div class = "titleOfferContainer flex">
+                        <span class = "dealTitleOffer"> ` + element.nome + ` <br> fibra óptica </span>
+                        <img src="imgs/MoreOffers_Arrow.svg" alt="">
+                    </div>
+                    <div class="dealsFeaturesIconsBox flex">
+                        <img src="imgs/OfferCardIcon_SmartPhone.svg" alt="">
+                        <img src="imgs/OfferCardIcon_Computer.svg" alt="">
+                        <img src="imgs/OfferCardIcon_GameConsole.svg" alt="">
+                        <img src="imgs/OfferCardIcon_HDVideo.svg" alt="">
+                        <img src="imgs/MoreOffers_Wifi.svg" alt="">
+                    </div>
+                    <div class="dealPriceContract flex">
+                        <span>por apenas</span>
+                        <span class = "dealPriceExta"> R$ ` + element.preco + ` </span>
+                        <a class="dealButton"
+                            href="https://api.whatsapp.com/send?phone=557530222334&text=Olá! Gostaria de contratar um produto com vocês, como podemos prosseguir?"
+                            target="_blank">Contratar</a>
+                    </div>
+                </div>
+            `;
+        }
+
+    });
+    replaceHtml.innerHTML = htmlPlansData;
 }
 
 function showMobileMenu(e) {
